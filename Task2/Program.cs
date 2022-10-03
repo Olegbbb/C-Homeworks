1b1f2eb0e3e5ed766cc1ba3a21d5904d03e6d08d
Oleg <apricot-ob@yandex.ru>
2022-10-03 18:35:48 +0300

    Задача2

---
+++ Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите первое число: ");
+double m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+double n = Convert.ToInt32(Console.ReadLine());
+if(m > n)
+{
+    Console.WriteLine("max = " + m);
+}
+else if (m < n)
+{
+    Console.WriteLine("max = " + n);
+}
+else if (m == n)
+{
+    Console.WriteLine("Числа равны");
+}
