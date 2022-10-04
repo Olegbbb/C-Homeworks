409ccc169a8c238f2fd2e0b1469040850b72020e
Oleg <apricot-ob@yandex.ru>
2022-10-04 23:23:11 +0300

    Задача8

---
+++ Task8/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a < 0)
+    Console.WriteLine("Введите положительное число");
+if (a == 0 || a == 1)
+    Console.WriteLine("Введите число больше 1");
+for (int i = 2; i <= a; i = i + 2)
+    Console.Write(i + "  ");
