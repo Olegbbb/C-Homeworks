9118d80cd167d7ded0b0bc4dec90ce4df048ecc3
Oleg <apricot-ob@yandex.ru>
2022-10-07 03:04:02 +0300

    Задача 13 - Второй семинар

---
+++ Task13/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine("Введите любое число");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n < 0)
+    n = n * (-1);
+if(n > 99)
+{
+    string ppp = (n).ToString();
+    Console.WriteLine(ppp[2]);
+}
+else 
+Console.WriteLine("Третьей цифры нет");
