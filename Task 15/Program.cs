5cd0fcc58ad9da99e3dc6b2eb5be02b0cecf5cc6
Oleg <apricot-ob@yandex.ru>
2022-10-07 03:30:47 +0300

    Задача 15 - Второй семинар

---
+++ Task 15/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.WriteLine("Введите номер дня недели");
+int n = Convert.ToInt32(Console.ReadLine());
+int p = 6;
+int k = 7;
+while (n < 1 || n > 7)
+{
+    Console.WriteLine("Вы ввели неверный номер");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+if (n == p || n == k)
+    Console.WriteLine("Выходной");
+else
+    Console.WriteLine("Не выходной");
+
+
