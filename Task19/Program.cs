a93ae7225b346f17ad923955f799f071ce13d5c9
Oleg <apricot-ob@yandex.ru>
2022-10-10 15:49:10 +0300

    Задача 19 - 3 Семинар

---
+++ Task19/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите пятизначное число");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a / 10000 == a % 10 && a / 1000 % 10 == a / 10 % 10)
+    Console.WriteLine("Yes");
+else
+    Console.WriteLine("No");
