179ec25d03e7a3f9b7cb31667c027a1e378c05ba
Oleg <apricot-ob@yandex.ru>
2022-10-11 21:58:32 +0300

    Задача 27 - 4 Семинар

---
+++ Task27/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.WriteLine("Введите любое число");
+int n = Convert.ToInt32(Console.ReadLine());
+n = Math.Abs(n);
+int digit = 0;
+int sum = 0;
+while(n > 0)
+{
+    digit = n % 10;
+    sum = sum + digit;
+    n = n / 10;
+}
+Console.WriteLine(sum);
