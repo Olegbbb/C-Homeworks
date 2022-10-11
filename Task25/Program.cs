aacd76f6ecea283c80d8f2e47d189f4bb676f30e
Oleg <apricot-ob@yandex.ru>
2022-10-11 20:48:58 +0300

    Задача 25 - 4 Семинар

---
+++ Task25/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число A");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B");
+int b = Convert.ToInt32(Console.ReadLine());
+int i = a;
+while(b > 1)
+{
+    a = a * i;
+    b = b - 1;
+}
+Console.WriteLine(a);
