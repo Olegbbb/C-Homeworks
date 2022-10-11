ed111427071a6bc47be507f0824fb2fd432e4d6b
Oleg <apricot-ob@yandex.ru>
2022-10-11 22:51:09 +0300

    Задача 29 - 4 Семинар

---
+++ Task29/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+int i = 0;
+int[] array = new int[8];
+for(i = 0; i < 8; i++)
+{
+    Console.WriteLine("Введите число массива");
+    int n = Convert.ToInt32(Console.ReadLine());
+    array [i] = n;
+}
+Console.WriteLine($"[{string.Join(", ", array)}]");
